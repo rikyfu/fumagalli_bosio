b756de672695882bd39c46da66203278184efaa5
Fra <vivenziofrancesco06@gmail.com>
2024-05-30 11:31:07 +0200

    modifica classe Pozione

--- pozione.py
+++ pozione.py
@@ -1,6 +1,20 @@
 from random import randint
 import pygame
 
+class Pozione:
+    def __init__(self, size):
+        self.image = pygame.image.load('immagini/pozione.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (size[0], size[1]))
+        self.rect = pygame.Rect(randint(50,550), -20, size[0], size[1])
+        self.speed_y = randint(5,8)
+
+    def update(self, screen):
+        self.rect.y += self.speed_y
+        if self.rect.y >= screen.get_height():
+            return True
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
 
     
